cbc565a85bea511152384b1dbdede47abb181bdb
MargoisMargo <29margo.lawley@boiseschools.net>
2026-02-03 13:54:08 -0700

    JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,5 @@
-
+function funky() {
+    var fun = prompt("Don't forget to me!")
+    var crab = prompt("Don't forget to me?")
+    var duck = prompt("Don't forget to me!")
+    alert(crab + fun + duck)
